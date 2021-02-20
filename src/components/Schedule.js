2f78aab697e36a6155206d92afb10c24ddefbeae
SgnvJaber <sgnvunleashed5@gmail.com>
2021-02-21 00:48:22 +0200

    add schedule filter table

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,13 +1,140 @@
-import React from 'react';
+  import React from 'react';
+import ReactDOM from 'react-dom';
+import FilterTable from 'react-filter-tables'
 import Nav from "./Nav"
 
 export default function Schedule(props) {
+  let data = [
+    {
+      'First Name': 1,
+      'Last Name': 'test',
+      'DATE': 'alan',
+      'Start Hour': 'abcd123',
+      'Finish Hour': 'abcd123',
+      'Project': 'abcd123',
+      'Description':"nedw",
+      'View images': 'images',
+    },
+    {
+      'First Name': 1,
+      'Last Name': 'test',
+      'DATE': 'alan',
+      'Start Hour': 'abcd123',
+      'Finish Hour': 'abcd123',
+      'Project': 'abcd123',
+      'Description':"nedw",
+      'View images': 'images',
+    },
+    {
+      'First Name': 1,
+      'Last Name': 'test',
+      'DATE': 'alan',
+      'Start Hour': 'abcd123',
+      'Finish Hour': 'abcd123',
+      'Project': 'abcd123',
+      'Description':"nedw",
+      'View images': 'images',
+    },
+    {
+      'First Name': 1,
+      'Last Name': 'test',
+      'DATE': 'alan',
+      'Start Hour': 'abcd123',
+      'Finish Hour': 'abcd123',
+      'Project': 'abcd123',
+      'Description':"nedw",
+      'View images': 'images',
+    },
+    {
+      'First Name': 1,
+      'Last Name': 'test',
+      'DATE': 'alan',
+      'Start Hour': 'abcd123',
+      'Finish Hour': 'abcd123',
+      'Project': 'abcd123',
+      'Description':"nedw",
+      'View images': 'images',
+    },
+    {
+      'First Name': 1,
+      'Last Name': 'test',
+      'DATE': 'alan',
+      'Start Hour': 'abcd123',
+      'Finish Hour': 'abcd123',
+      'Project': 'abcd123',
+      'Description':"nedw",
+      'View images': 'images',
+    },
+    {
+      'First Name': 1,
+      'Last Name': 'test',
+      'DATE': 'alan',
+      'Start Hour': 'abcd123',
+      'Finish Hour': 'abcd123',
+      'Project': 'abcd123',
+      'Description':"nedw",
+      'View images': 'images',
+    },
+    {
+      'First Name': 1,
+      'Last Name': 'test',
+      'DATE': 'alan',
+      'Start Hour': 'abcd123',
+      'Finish Hour': 'abcd123',
+      'Project': 'abcd123',
+      'Description':"nedw",
+      'View images': 'images',
+    },
+    {
+      'First Name': 1,
+      'Last Name': 'test',
+      'DATE': 'alan',
+      'Start Hour': 'abcd123',
+      'Finish Hour': 'abcd123',
+      'Project': 'abcd123',
+      'Description':"nedw",
+      'View images': 'images',
+    },    
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  ]
+
+
+
+
+
+
+
+
+
   return (
    <div>
           <Nav/>
           <h1> i am the schedule</h1>
           <h1>{props.token}</h1>
-
+          <FilterTable data={data}
+          onClick = { ( rowObject ) => {
+          console.log( rowObject );
+            }}
+          recordsPerPage = { 8 }
+          style = {{ position:'center', minWidth: '100%',}}
+          classNames = {["your_css_class_1", "your_css_class_2"]} />
+       
+          
+          
+          
+          
+          
+          
    </div>
   );
 }
