ca2de39cc281a23c4df74feb34d19630cb8fa76d
ahmad sh <ahmad.sharabati.96@gmail.com>
2021-04-29 15:24:29 +0300

    preparing schedule page

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -4,41 +4,9 @@ import FilterTable from "react-filter-tables";
 import Nav from "./Nav";
 
 export default function Schedule(props) {
+  var toke = "Token " + props.token + " ";
   const [data, setData] = React.useState([]);
-  let BACKEND_URL = "https://hashmali-backend.herokuapp.com/api/report/";
-  const fetchReports = () => {
-    let url = new URL(BACKEND_URL);
-    fetch(url, {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((resp) => {
-        console.log("res " + JSON.stringify(resp));
-        setData(...data, resp);
-      })
-      .catch((error) => {
-        console.log("ERROR ", error);
-      });
-  };
-  // let data = [
-  //   {
-  //     "First Name": 1,
-  //     "Last Name": "test",
-  //     DATE: "alan",
-  //     "Start Hour": "abcd123",
-  //     "Finish Hour": "abcd123",
-  //     Project: "abcd123",
-  //     Description: "nedw",
-  //     "View images": "images",
-  //     Action: "images",
-  //   },
-  // ];
 
-  useEffect(() => {
-    fetchReports();
-  }, []);
-
-  console.log("data is " + data);
   return (
     <div>
       <Nav activeItem="Schedule" />
@@ -49,7 +17,7 @@ export default function Schedule(props) {
         onClick={(rowObject) => {
           console.log(rowObject);
         }}
-        recordsPerPage={8}
+        recordsPerPage={50}
         style={{ position: "center", minWidth: "100%" }}
         classNames={["your_css_class_1", "your_css_class_2"]}
       />
