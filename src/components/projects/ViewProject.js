91864919007ad7340bbcb86fecda941dfc9e821a
SgnvJaber <sgnvunleashed5@gmail.com>
2021-06-09 00:41:00 +0300

    add icons to view project

--- src/components/projects/ViewProject.js
+++ src/components/projects/ViewProject.js
@@ -2,7 +2,25 @@ import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import Loader from "../Loader";
 import Avatar from "../Avatar";
-
+import ApartmentIcon from "@material-ui/icons/Apartment";
+import HomeIcon from "@material-ui/icons/Home";
+import LocationCityIcon from "@material-ui/icons/LocationCity";
+import LocationSearchingIcon from "@material-ui/icons/LocationSearching";
+import DomainIcon from "@material-ui/icons/Domain";
+import GpsFixedIcon from "@material-ui/icons/GpsFixed";
+import AddLocationIcon from "@material-ui/icons/AddLocation";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import GroupIcon from "@material-ui/icons/Group";
+import ContactPhoneIcon from "@material-ui/icons/ContactPhone";
+import ContactMailIcon from "@material-ui/icons/ContactMail";
+import PermContactCalendarSharpIcon from "@material-ui/icons/PermContactCalendarSharp";
+import ContactsSharpIcon from "@material-ui/icons/ContactsSharp";
+import LocalPhoneSharpIcon from "@material-ui/icons/LocalPhoneSharp";
+import EmailSharpIcon from "@material-ui/icons/EmailSharp";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
 const Project = (props) => {
   const [items, setItems] = useState([]);
   const [status, setStatus] = useState("");
@@ -52,75 +70,116 @@ const Project = (props) => {
         Project Code: {items.project_code}
       </h1>
       <hr />
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <ul className="list-group w-50">
-          <li className="list-group-item">
-            Property Type: {items.property_type}
-          </li>
-          <li className="list-group-item">Address: {items.address_link}</li>
-          <li className="list-group-item">City: {items.city}</li>
-          <li className="list-group-item">Street: {items.street}</li>
-          <li className="list-group-item">property_no: {items.property_no}</li>
-        </ul>
+      <div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <ul className="list-group w-25">
+            <h1 style={{ textAlign: "center" }}>Building</h1>
 
-        <ul className="list-group w-50">
-          <li className="list-group-item">
-            <a
-              href={`https://www.waze.com/live-map?utm_source=waze_website&utm_campaign=waze_website&utm_medium=website_menu`}
-              target="_blank"
-            >
-              Open Waze
-            </a>
-          </li>
-          <li className="list-group-item">
-            <a href={`https://www.google.co.il/maps`} target="_blank">
-              Open Google Maps
-            </a>
-          </li>
-        </ul>
+            <li className="list-group-item">
+              <ApartmentIcon />
+              <strong> Property Type:</strong> {items.property_type}
+            </li>
+            <li className="list-group-item">
+              {" "}
+              <HomeIcon />
+              <strong>Address:</strong> {items.address_link}
+            </li>
+            <li className="list-group-item">
+              {" "}
+              <LocationCityIcon />
+              <strong>City:</strong> {items.city}
+            </li>
+            <li className="list-group-item">
+              {" "}
+              <LocationSearchingIcon />
+              <strong>Street:</strong> {items.street}
+            </li>
+            <li className="list-group-item">
+              {" "}
+              <DomainIcon />
+              <strong>property_no:</strong> {items.property_no}
+            </li>
+            <li className="list-group-item">
+              <GpsFixedIcon />
+              <a
+                href={`https://www.waze.com/live-map?utm_source=waze_website&utm_campaign=waze_website&utm_medium=website_menu`}
+                target="_blank"
+              >
+                Open Waze
+              </a>
+            </li>
+            <li className="list-group-item">
+              <AddLocationIcon />
+              <a href={`https://www.google.co.il/maps`} target="_blank">
+                Open Google Maps
+              </a>
+            </li>
+          </ul>
 
-        <ul className="list-group w-50" style={{ marginLeft: "15px" }}>
-          <li className="list-group-item">
-            Owner First Name: {items.owner_first_name}
-          </li>
-          <li className="list-group-item">
-            Owner Last Name: {items.owner_second_name}
-          </li>
-          <li className="list-group-item">
-            Owner Phone: {items.owner_phone_no}
-          </li>
-          <li className="list-group-item">Owner Email: {items.owner_email}</li>
-        </ul>
+          <ul className="list-group w-25" style={{ marginLeft: "15px" }}>
+            <h1 style={{ textAlign: "center" }}>Owner</h1>
 
-        <ul className="list-group w-50" style={{ marginLeft: "15px" }}>
-          <li className="list-group-item">
-            Contractor First Name: {items.contractor_first_name}
-          </li>
-          <li className="list-group-item">
-            Contractor Last Name: {items.contractor_second_name}
-          </li>
-          <li className="list-group-item">
-            Contractor Phone: {items.contractor_phone_no}
-          </li>
-          <li className="list-group-item">
-            Contractor Email: {items.contractor_email}
-          </li>
-        </ul>
+            <li className="list-group-item">
+              <AccountBoxIcon />
+              <strong> First Name:</strong> {items.owner_first_name}
+            </li>
+            <li className="list-group-item">
+              <GroupIcon />
+              <strong> Last Name:</strong> {items.owner_second_name}
+            </li>
+            <li className="list-group-item">
+              <ContactPhoneIcon />
+              <strong> Phone:</strong> {items.owner_phone_no}
+            </li>
+            <li className="list-group-item">
+              <ContactMailIcon />
+              <strong> Email:</strong> {items.owner_email}
+            </li>
+          </ul>
+          <ul className="list-group w-25" style={{ marginLeft: "15px" }}>
+            <h1 style={{ textAlign: "center" }}>Contractor </h1>
 
-        <ul className="list-group w-50" style={{ marginLeft: "15px" }}>
-          <li className="list-group-item">
-            Architect First Name: {items.architect_first_name}
-          </li>
-          <li className="list-group-item">
-            Architect Last Name: {items.architect_second_name}
-          </li>
-          <li className="list-group-item">
-            Architect Phone: {items.architect_phone_no}
-          </li>
-          <li className="list-group-item">
-            Architect Email: {items.architect_email}
-          </li>
-        </ul>
+            <li className="list-group-item">
+              <PermContactCalendarSharpIcon />
+              <strong> First Name:</strong> {items.contractor_first_name}
+            </li>
+            <li className="list-group-item">
+              <ContactsSharpIcon />
+              <strong> Last Name:</strong> {items.contractor_second_name}
+            </li>
+            <li className="list-group-item">
+              <LocalPhoneSharpIcon />
+              <strong> Phone:</strong> {items.contractor_phone_no}
+            </li>
+            <li className="list-group-item" style={{ fontSize: "12px" }}>
+              <EmailSharpIcon />
+              <strong> Email:</strong> {items.contractor_email}
+            </li>
+          </ul>
+          <ul className="list-group w-25" style={{ marginLeft: "15px" }}>
+            <h1 style={{ textAlign: "center" }}>Architect </h1>
+            <li className="list-group-item">
+              <AccountCircleIcon />
+              <strong> First Name:</strong> {items.architect_first_name}
+            </li>
+            <li className="list-group-item">
+              <SupervisedUserCircleIcon />
+              <strong> Last Name:</strong> {items.architect_second_name}
+            </li>
+            <li className="list-group-item">
+              <PhoneAndroidIcon />
+              <strong> Phone:</strong> {items.architect_phone_no}
+            </li>
+            <li className="list-group-item">
+              <MailOutlineIcon />
+              <strong> Email:</strong> {items.architect_email}
+            </li>
+          </ul>
+        </div>
+
+        <div
+          style={{ display: "flex", flexDirection: "row", marginTop: "10px" }}
+        ></div>
       </div>
     </div>
   );
